e77d54cf40c56d3a1d94ed20b1a05253d560259b
manikantan-nyros <manikantan_nyros@yahoo.com>
2015-12-03 12:16:46 +0530

    added file2

--- wercksdemo/tests.py
+++ wercksdemo/tests.py
@@ -1,5 +1,10 @@
 from django.test.client import Client
 from django.test import TestCase
+import re
+
+from django.test import LiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+
 
 
 class HomeTest(TestCase):
@@ -18,3 +23,25 @@ class HomeTest(TestCase):
         response = c.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Amsterdam")
+
+
+class MySeleniumTests(LiveServerTestCase):
+    fixtures = ["data/locations.json"]
+
+    @classmethod
+    def setUpClass(cls):
+        cls.selenium = WebDriver()
+        super(MySeleniumTests, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super(MySeleniumTests, cls).tearDownClass()
+
+    def test_locations(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/locations/'))
+        src = self.selenium.page_source
+        text_found = re.search(r'text_to_search', src)
+        self.assertFalse(text_found)
+        text_found = re.search(r'Amsterdam', src)
+        self.assertTrue(text_found)
